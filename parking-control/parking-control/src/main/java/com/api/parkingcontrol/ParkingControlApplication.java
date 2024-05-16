2ef5756af21bcaf16475859c674e6bf1c786cfcb
DAVID CHRISTIAN MACHADO <david.m.cristian@gmail.com>
2024-05-16 10:36:21 -0300

    Implementação de Validações e Metodos

--- parking-control/parking-control/src/main/java/com/api/parkingcontrol/ParkingControlApplication.java
+++ parking-control/parking-control/src/main/java/com/api/parkingcontrol/ParkingControlApplication.java
@@ -1,13 +1,28 @@
 package com.api.parkingcontrol;
 
+import com.api.parkingcontrol.dtos.ParkingSpotDto;
+import com.api.parkingcontrol.models.ParkingSpotModel;
+import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 @SpringBootApplication
 @RestController
 public class ParkingControlApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(ParkingControlApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(ParkingControlApplication.class, args);
@@ -15,7 +30,10 @@ public class ParkingControlApplication {
 
 	@GetMapping("/")
 	public String inddex(){
+//		System.out.println("teste");
 		return "Olá Mundo!";
 	}
+
+
 
 }
